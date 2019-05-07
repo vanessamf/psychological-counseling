c3d568c7ec47cf043246523c6481066975a4cf86
Vanessamf <78355655cc@sina.cn>
2019-05-07 16:04:19 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+$(function() {
+    $('.weui-tabbar__item').on('click', function() {
+//      $(this).addClass('weui-bar__item_on').siblings('.weui-bar__item_on').removeClass('weui-bar__item_on');
+    });
+});
